347e603dc380a68e9ea02b25e8513f8f9d6eee97
Stephan Arnas <stephan.arnas@gmail.com>
2025-07-14 14:32:48 +0200

    fixes: iOS line height issue
    Addresses an iOS-specific line height issue introduced in a recent Xcode version.
    
    Adds a workaround to prevent exceptions when setting the line height on labels.
    This ensures correct text rendering on iOS devices.

--- src/CraftUI.Demo/Platforms/iOS/AppDelegate.cs
+++ src/CraftUI.Demo/Platforms/iOS/AppDelegate.cs
@@ -5,5 +5,20 @@ namespace CraftUI.Demo;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+    {
+        // Fix for iOS line height issue on latest XCode version.
+        // https://github.com/dotnet/maui/issues/30609
+        Microsoft.Maui.Handlers.LabelHandler.Mapper.ModifyMapping(nameof(ILabel.LineHeight),
+            (handler, label, action) =>
+            {
+                // Skip line height mapping if the value would cause an exception
+                if (label.LineHeight > 0)
+                {
+                    action(handler, label);
+                }
+            });
+
+        return MauiProgram.CreateMauiApp();
+    }
 }
